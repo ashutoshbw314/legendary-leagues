8c71f4751670d315521298ce9b44b6a18eb9f7e4
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-03-10 15:54:25 +0600

    removed the troublemaker useEffect and it works like ✨

--- src/hooks/useJSON.js
+++ src/hooks/useJSON.js
@@ -1,4 +1,4 @@
-import {useReducer, useEffect} from "react";
+import {useReducer} from "react";
 
 const initialState = {
   loading: true,
@@ -27,21 +27,18 @@ const reducer = (state, action) => {
 
 function useJSON(url, keys){
   const [state, dispatch] = useReducer(reducer, initialState);
-  useEffect(() => {
-    const fetchData = async (url) => {
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        const nestedData = keys.reduce((nested, key) => nested[key], data);
-        dispatch({type: 'success', data: nestedData});
-      } catch (error) {
-        dispatch({type: 'error', errorMessage: error.message});
-      }
+  const fetchData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      const nestedData = keys.reduce((nested, key) => nested[key], data);
+      dispatch({type: 'success', data: nestedData});
+    } catch (error) {
+      dispatch({type: 'error', errorMessage: error.message});
     }
+  }
 
-    fetchData(url);
-
-  }, [])
+  fetchData(url);
 
   return [state.loading, state.error, state.data];
 }
